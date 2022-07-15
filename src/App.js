00553cf9a7081d07c35ed600b5c1182445ab285b
Rob Thijssen <rthijssen@gmail.com>
2022-07-15 12:55:22 +0300

    timeline

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
+import { dateDiff } from './utils';
 
-function App() {
+const isoDatePattern = new RegExp('^(199[0-9]|20[0-2][0-9])-[01][0-9]-[0-3][0-9]$');
+const dateFormat = (d) => (
+  (isoDatePattern.test(d))
+    ? new Intl.DateTimeFormat('default', { year: 'numeric', month: 'long' }).format(new Date(d)).toLowerCase()
+    : d
+);
+const timespanFormat = (from, to) => (
+  (from === to || !to)
+    ? dateFormat(from)
+    : `${dateFormat(from)} - ${dateFormat(to)}`
+);
+const flag = (countryCode) => (
+  String.fromCodePoint(...countryCode
+    .toUpperCase()
+    .split('')
+    .map(char =>  127397 + char.charCodeAt()))
+);
+
+
+const App = () => {
+  const [timeline, setTimeline] = useState(undefined);
+  useEffect(() => {
+    fetch(`https://gist.githubusercontent.com/grenade/c80d872e98b8b38401c92cc468b91003/raw/timeline.json`)
+      .then(response => response.json())
+      .then((timeline) => {
+        setTimeline(
+          timeline
+            .filter((x) => ['home', 'work'].includes(x.type) && ((x.to === 'present') || (dateDiff(new Date(x.from), new Date(x.to), 'day') > 31)))
+            .sort((a, b) => (a.from > b.from) ? 1 : (a.from < b.from) ? -1 : 0)
+            .reduce((a, x) => {
+              if ((!!a.length) && (a[a.length - 1].town === x.town)) {
+                const l = a.pop();
+                return [...a, ...[{
+                  ...x,
+                  from: l.from,
+                }]];
+              } else {
+                return [...a, ...[x]];
+              }
+            }, [])
+            //.reverse()
+        );
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Container>
+      <h1>rob thijssen</h1>
+      <h2>residency timeline</h2>
+      <h3>1992 ~ 2022</h3>
+      {
+        (!!timeline)
+          ? (
+              <Table>
+                <thead>
+                  <tr>
+                    <th>date</th>
+                    <th>location</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    timeline.map((x, xI) => (
+                      <tr key={xI}>
+                        <td>
+                          {timespanFormat(x.from, x.to)} <sup className="text-muted">{dateDiff(new Date(x.from), new Date((x.to === 'present') ? new Date() : x.to), 2)}</sup>
+                        </td>
+                        <td>
+                          {`${flag(x.country.code)} ${x.town.toLowerCase()}, ${x.country.name.toLowerCase().replace('united kingdom', 'uk')}`}
+                        </td>
+                      </tr>
+                    ))
+                  }
+                </tbody>
+              </Table>
+            )
+          : null
+      }
+    </Container>
+  )
+};
 
 export default App;
---
+++ src/utils.js
@@ -0,0 +1,50 @@
+
+export function dateDiff(a, b, output = 'object') {
+  if (b === null || b === undefined) {
+    b = new Date(new Date().getTime()); // utc now
+  }
+  const seconds = {
+    year: 31536000,
+    month: 2592000,
+    week: 604800,
+    day: 86400,
+    hour: 3600,
+    minute: 60,
+    second: 1
+  };
+  let delta = Math.abs((a - b) / 1000);
+  let diff = {};
+  Object.keys((seconds)).filter((p, pI) => Number.isInteger(output) ? pI <= output : true).forEach((period) => {
+    diff[period] = Math.floor(delta / seconds[period]);
+    delta -= diff[period] * seconds[period];
+  });
+  switch (output) {
+    case 'object':
+      return diff;
+    case 'significant':
+    case 1:
+      return (diff.year > 0)
+        ? `${diff.year} year${(diff.year > 1) ? 's' : ''}`
+        : (diff.month > 0)
+          ? `${diff.month} month${(diff.month > 1) ? 's' : ''}`
+          : (diff.week > 0)
+            ? `${diff.week} week${(diff.week > 1) ? 's' : ''}`
+            : (diff.day > 0)
+              ? `${diff.day} day${(diff.day > 1) ? 's' : ''}`
+              : (diff.hour > 0)
+                ? `${diff.hour} hour${(diff.hour > 1) ? 's' : ''}`
+                : (diff.minute > 0)
+                  ? `${diff.minute} minute${(diff.minute > 1) ? 's' : ''}`
+                  : (diff.second > 0)
+                    ? `${diff.second} second${(diff.second > 1) ? 's' : ''}`
+                    : '';
+    case 'day':
+      return ((diff.year * seconds.year / seconds.day) + (diff.month * seconds.month / seconds.day) + (diff.week * seconds.week / seconds.day) + diff.day);
+    case 'hour':
+      return ((diff.year * seconds.year / seconds.hour) + (diff.month * seconds.month / seconds.hour) + (diff.week * seconds.week / seconds.hour) + (diff.day * seconds.day / seconds.hour) + diff.hour);
+    default:
+      return Number.isInteger(output)
+        ? Object.keys(diff).map((period) => `${(diff[period] > 0) ? `${diff[period]} ${period}${(diff[period] > 1) ? 's' : ''}`: ''}`).filter((x) => !!x).slice(0, output).join(', ')
+        : Object.keys(diff).map((period) => `${(diff[period] > 0) ? `${diff[period]} ${period}${(diff[period] > 1) ? 's' : ''}`: ''}`).filter((x) => !!x).join(', ');
+  }
+}
